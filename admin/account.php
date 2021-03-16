21aeff1a3d5d5ae6b1a2533762ba928e7de0c54a
kiwizu3 <kavinda@sqrmobile.com>
2021-03-17 04:02:22 +0530

    Account

--- admin/account.php
+++ admin/account.php
@@ -1,5 +1,8 @@
 <!doctype html>
 <html lang="en">
+<?php
+include 'session.php';
+?>
 
 <head>
     <meta charset="utf-8">
@@ -47,94 +50,14 @@
         <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
-                <a class="nav-link" href="../login.php">Sign out</a>
+       <a class="nav-link" href="../logout.php"><?php echo $login_session; ?> | Sign out</a>
             </li>
         </ul>
     </header>
 
     <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div class="position-sticky pt-3">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.php">
-                                <span data-feather="home"></span>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="members.php">
-                                <span data-feather="file"></span>
-                                Members
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="transactions.php">
-                                <span data-feather="shopping-cart"></span>
-                                Transactions
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="chats.php">
-                                <span data-feather="users"></span>
-                                Chats
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="content.php">
-                                <span data-feather="layers"></span>
-                                Content
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="account.php">
-                                <span data-feather="bar-chart-2"></span>
-                                Account
-                            </a>
-                        </li>
-                        <!-- <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
-            </a>
-          </li> -->
-                    </ul>
-
-                    <!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
-          <a class="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Current month
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Last quarter
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Social engagement
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Year-end sale
-            </a>
-          </li>
-        </ul> -->
-                </div>
-            </nav>
+            <?php include("navigation.php") ?>
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -146,7 +69,7 @@
                         No Data Found
                     </p>
                 </div>
-              
+
             </main>
         </div>
     </div>
